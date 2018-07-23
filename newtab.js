234532b249cf1d68092f9b533862dad9b19caa48
SquareDorito <khnoh@brown.edu>
2018-07-22 22:25:32 -0700

    runing date working

--- newtab.js
+++ newtab.js
@@ -4,20 +4,35 @@ document.addEventListener("DOMContentLoaded", function () {
 
 function formatDate(date) {
     var monthNames = [
-      "January", "February", "March",
-      "April", "May", "June", "July",
-      "August", "September", "October",
-      "November", "December"
+        "January", "February", "March",
+        "April", "May", "June", "July",
+        "August", "September", "October",
+        "November", "December"
     ];
-  
+
     var day = date.getDate();
     var monthIndex = date.getMonth();
     var year = date.getFullYear();
-  
-    return monthNames[monthIndex]+' '+day + ', ' + year;
-  }
+
+    return monthNames[monthIndex] + ' ' + day + ', ' + year;
+}
+
+var start = new Date('04/15/1998 4:00 PM');
+var timer;
+
+function age() {
+    var now = new Date();
+    var age = now.getTime() - start.getTime();
+    var year = (age / 31556926000); // seconds in a year * 1000
+    $('#age').html("You are "+"<strong>"+year.toFixed(9)+"</strong> years old.");
+}
 
 var getAllCallback = function (list) {
+    timer = setInterval(age, 1);
+    $("#time").empty()
+    var date = "Todays Date: <strong>" + formatDate(new Date())+"</strong>.";
+    $("#time").append(date)
+
     var apps = document.getElementById("apps");
     for (var i in list) {
         // we don't want to do anything with extensions yet.
@@ -44,9 +59,6 @@ var getAllCallback = function (list) {
             apps.appendChild(app);
         }
     }
-    $("#time").empty()
-    var date = "Todays Date: "+formatDate(new Date());
-    $("#time").append(date)
 };
 
 var find128Image = function (icons) {
