550043c2efe56df4fc38e91ebf66e2586171239a
kennoh <khnoh@brown.edu>
2018-11-06 13:47:16 -0500

    basic dragging working, need to store the orientation

--- js/newtab.js
+++ js/newtab.js
@@ -110,6 +110,11 @@ var getAllCallback = function (list) {
         }
     });*/
 
+    chrome.history.search({text: '', maxResults: 10}, function(data) {
+        data.forEach(function(page) {
+            console.log(page.url);
+        });
+    });
 
 
     function handleDragStart(e) {
